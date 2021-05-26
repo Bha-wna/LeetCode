9eb5ebae34a5d72aceceab9be21bd63fc32a770e
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2021-05-26 17:40:05 +0000

    Sync LeetCode submission - Kids With the Greatest Number of Candies (java)

---
+++ my-folder/problems/kids_with_the_greatest_number_of_candies/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        
+        List<Boolean> greatest = new ArrayList<>();
+        int max = candies[0];
+        for(int i = 0; i< candies.length; i++)
+        {
+           if(candies[i] > max)
+           { 
+               max = candies[i];
+           }
+        }
+        
+        for(int i = 0; i<candies.length; i++)
+        {
+           if(candies[i] + extraCandies >= max) 
+           {
+              greatest.add(true); 
+           }
+            else
+                greatest.add(false);
+            
+        }
+         return greatest;
+    }
+}
