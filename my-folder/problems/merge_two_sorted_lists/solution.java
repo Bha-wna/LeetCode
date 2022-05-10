60f9926ac8d29cf121f44c6539ee25f808178b4e
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-05-10 01:04:07 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (java)

--- my-folder/problems/merge_two_sorted_lists/solution.java
+++ my-folder/problems/merge_two_sorted_lists/solution.java
@@ -8,43 +8,39 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+import java.util.*;
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode f = list1;
+        ListNode s = list2;
         ListNode ans = new ListNode();
-        ListNode head = ans;
+        ListNode ret = ans;
         
-        while(list1 != null && list2 !=null)
+        while(list1 != null && list2 != null)
         {
-            if(list1.val<= list2.val)
+            if(list1.val< list2.val)
             {
-               ListNode temp = new ListNode(list1.val);
-                ans.next = temp;
-                ans = ans.next;
-                list1 = list1.next;
+                ans.next =list1;
+                list1 = list1.next; 
             }
             else
-            {
-                ListNode temp = new ListNode(list2.val);
-                ans.next = temp;
-                ans = ans.next;
-                list2 = list2.next;
+            {  ans.next = list2;
+                list2 =  list2.next;
             }
+            ans = ans.next;
         }
-        
-            while(list2 != null)
-            {
-                ListNode temp = new ListNode(list2.val);
-                ans.next = temp;
-                ans = ans.next;
-                list2 = list2.next; 
-            }
-            while(list1 !=null)
-            {
-                ListNode temp = new ListNode(list1.val);
-                ans.next = temp;
-                ans = ans.next;
-                list1 = list1.next;
-            }
-        return head.next;
+        while(list1 != null)
+        {
+            ans.next = list1;
+            list1= list1.next;
+            ans = ans.next;
+        }
+         while(list2 != null)
+        {
+            ans.next = list2;
+            list2= list2.next;
+            ans = ans.next;
+        }
+        return ret.next;
     }
 }
