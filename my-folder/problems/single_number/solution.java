bf0f204cec08dec2a22c05ff13edbeb425fa09f5
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-07-03 18:23:41 +0000

    Sync LeetCode submission - Single Number (java)

--- my-folder/problems/single_number/solution.java
+++ my-folder/problems/single_number/solution.java
@@ -1,11 +1,8 @@
 class Solution {
-    public int singleNumber(int[] nums) 
-    {
-       int n =0;
+    public int singleNumber(int[] nums) {
+        int xor = 0;
         for(int i=0; i<nums.length; i++)
-        {
-            n = n^nums[i];
-        }
-        return n;
+            xor = xor ^nums[i];
+        return xor;
     }
 }
