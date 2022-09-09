d00b4d9954e9e9ba3b2eb88439f4cfe408509c9a
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-09-09 06:24:01 +0000

    Sync LeetCode submission - Running Sum of 1d Array (java)

--- my-folder/problems/running_sum_of_1d_array/solution.java
+++ my-folder/problems/running_sum_of_1d_array/solution.java
@@ -1,32 +1,12 @@
 class Solution {
-    int k=0;
-    int j=0;
-   // int i=0;
-   // int d = nums.length;
-      
-    public int[] runningSum(int[] nums) 
-    {
-        int[] arr = new int[nums.length];
-        for(int i=0; i<nums.length; i++)
-        { 
-           
-            if( i==0)
-           {
-             nums[i]= nums[i];
-           }
-           else
-           {
-            for(j=0; j<1; j++)
-            { 
-             nums[i]= nums[i]+ arr[k-1]  ;
-            }
-           }    
-           arr[k]= nums[i];
-            k++;
-         
-          // k[i]= nums[i];
-           // arr = k; 
+    public int[] runningSum(int[] nums) {
+        int sum=0;
+        int ans[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            sum=sum+nums[i];
+            ans[i]=sum;
         }
-        return arr;
+        return ans;
     }
 }
