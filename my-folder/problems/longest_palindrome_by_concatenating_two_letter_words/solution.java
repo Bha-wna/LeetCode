9b869be1d2deec2c04ac8eabf96315c153789af2
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-11-03 14:56:54 +0000

    Sync LeetCode submission - Longest Palindrome by Concatenating Two Letter Words (java)

---
+++ my-folder/problems/longest_palindrome_by_concatenating_two_letter_words/solution.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+       Map<String, Integer> m = new HashMap<>();
+        int count =0;
+        for(int i=0; i<words.length; i++)
+        {
+            String revString = rev(words[i]);
+            if(m.containsKey(revString) && m.get(revString) != 0){
+                count += 4;
+                m.put(revString, m.get(revString) - 1);
+            } else{
+                m.put(words[i], m.getOrDefault(words[i], 0) + 1);
+            }
+        }
+        for(Map.Entry<String, Integer> entry : m.entrySet())
+        {
+            if(isPalindrome(entry.getKey()) && entry.getValue() > 0){   
+                count += 2;
+                break;
+            }
+        }
+        return count;
+    }
+    public static String rev(String str)
+    {
+        String temp ="";
+        for(int i=str.length()-1; i>=0; i--)
+        {
+            temp += str.charAt(i);
+        }
+        return temp;
+    }
+    public static boolean isPalindrome(String str)
+    {
+        int i=0; 
+        int j= str.length()-1;
+        while(i<j)
+        {
+            if(str.charAt(i)!= str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
