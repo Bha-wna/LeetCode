b8ba8cd24974ae4ed23a03b6b37b2baa795171f7
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-11-04 07:26:02 +0000

    Sync LeetCode submission - Contains Duplicate (java)

--- my-folder/problems/contains_duplicate/solution.java
+++ my-folder/problems/contains_duplicate/solution.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       Map<Integer, Integer> m = new HashMap<>();
-        for(int i=0; i<nums.length; i++)
+       Arrays.sort(nums);
+        for(int i=0; i<nums.length-1; i++)
         {
-            if(m.containsKey(nums[i]))
-                m.put(nums[i],m.get(nums[i])+1 );
-            else
-                m.put(nums[i],1);
-        }
-        for(int i : m.values())
-        {
-            if(i >1)
+            if(nums[i] == nums[i+1])
                 return true;
         }
         return false;
