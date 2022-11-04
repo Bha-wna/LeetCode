41a1cfcf1939241cffb690ae6a24583dd6551c19
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-11-04 17:14:31 +0000

    Sync LeetCode submission - Reverse Words in a String (java)

--- my-folder/problems/reverse_words_in_a_string/solution.java
+++ my-folder/problems/reverse_words_in_a_string/solution.java
@@ -1,53 +1,33 @@
 class Solution {
     public String reverseWords(String s) {
-        // String ans ="";
-        // String temp ="";
-        // List<String> arr = new ArrayList<>();
-        // for(int i=s.length()-1; i>=0; i--)
-        // { // System.out.println(i);
-        //     if(i==0)
-        //     {
-        //         temp += s.charAt(i);
-        //         arr.add(temp);
-        //     }
-        //     if(s.charAt(i)==' ')
-        //     {
-        //         arr.add(temp);
-        //         temp ="";
-        //         continue;
-        //     }
-        //     temp += s.charAt(i);
-        // }
-        // System.out.println(arr);
-        // // for(int i=0)
-        // return " ";
-        boolean flag = false;
-        String temp ="";
-        ArrayList<String> ass = new ArrayList<>();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) != ' '){
-                temp += s.charAt(i);
-                flag = true;
-            } else{
-                if(temp != "")
-                    ass.add(temp);
-                temp = "";
-                flag = false;
-            }
-        }
-        if(flag)
-            ass.add(temp);
-        System.out.println(ass);
-      //  return "";
         
-        String ans ="";
-        for(int i=ass.size()-1; i>=0; i--)
+    Stack <String> st = new Stack<>();
+        String ans = "";
+
+        int len = s.length();
+        String temp ="";
+        for(int i=0; i<len; i++)
         {
-            if(i==0)
-                ans+= ass.get(i);
-            else
-              ans+= ass.get(i) + " ";
+           
+           if(s.charAt(i) !=' ')
+           {   
+               temp += s.charAt(i);
+              // System.out.println(temp);
+               if(i == len-1)
+                   st.push(temp);
+           }
+           else if(temp != "")
+           {   st.push(temp);
+               temp = "";
+           }
         }
-        return ans;
-    }
+        System.out.println(st);
+         while(!st.isEmpty())
+         {
+            ans += st.pop() + " ";
+         }
+       // System.out.println(ans);
+        return ans.substring(0,ans.length()-1);
+       // return ans;
+  }
 }
