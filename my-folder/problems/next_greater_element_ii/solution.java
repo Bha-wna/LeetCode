8120b8209dd4f212807bc385585e704150cd4aa1
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-11-08 14:09:53 +0000

    Sync LeetCode submission - Next Greater Element II (java)

--- my-folder/problems/next_greater_element_ii/solution.java
+++ my-folder/problems/next_greater_element_ii/solution.java
@@ -2,12 +2,14 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int [] ans = new int[n];
+        int count =0;
         Stack<Integer> st = new Stack<>();
         for(int i=2*n-1; i>=0; i--)
         {
             while(!st.isEmpty() && st.peek() <= nums[i%n])
             {
                 st.pop();
+                System.out.println(count++);
             }
             if(i<n)
             {
