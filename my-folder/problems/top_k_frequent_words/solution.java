e3151437aab962acce1f4068c59b3dce4d9e890c
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-11-09 15:22:43 +0000

    Sync LeetCode submission - Top K Frequent Words (java)

--- my-folder/problems/top_k_frequent_words/solution.java
+++ my-folder/problems/top_k_frequent_words/solution.java
@@ -1,56 +1,31 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        Map<String, Integer> m = new HashMap<>();
+        
+        List<String> result = new LinkedList<>();
+        Map<String, Integer> map = new HashMap<>();
         for(int i=0; i<words.length; i++)
         {
-            if(!m.containsKey(words[i]))
-                m.put(words[i],1);
+            if(map.containsKey(words[i]))
+                map.put(words[i], map.get(words[i])+1);
             else
-                m.put(words[i],m.get(words[i])+1);
+                map.put(words[i], 1);
         }
         
-        LinkedHashMap<String,Integer> rm = new LinkedHashMap<>();
-        m.entrySet()
-  .stream()
-  .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) 
-  .forEachOrdered(x -> rm.put(x.getKey(), x.getValue()));
-        
-       System.out.println(rm);
-        List<String> ans = new ArrayList<>();
-        List<String> result = new ArrayList<>();
-        
-        Map.Entry<String, Integer> entry = rm.entrySet().iterator().next();
-        int prev = entry.getValue();
-       
-            for (Map.Entry<String, Integer> ite : rm.entrySet()) 
-            {
-              // int prev = ite.get(0); 
-              if(ite.getValue() != prev) 
-              {
-                List<String> temp = ans;
-                Collections.sort(temp);
-                for(String it: temp)
-                    result.add(it);
-                  
-                 ans.clear();
-                prev = ite.getValue();
-              }
-
-             ans.add(ite.getKey());
-            }
-    
-        Collections.sort(ans);
-        for(String it: ans)
-            result.add(it);
-        
-        List<String> final_result = new ArrayList<>();
-        for(String it: result)
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+                 (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
+        );
+        System.out.println(map);
+        for(Map.Entry<String, Integer> entry: map.entrySet())
         {
-            if(k==0)
-                break;
-            final_result.add(it);
-            k--;
+            pq.offer(entry);
+            if(pq.size()>k)
+                pq.poll();
         }
-        return final_result;
+        
+        System.out.println(pq);
+        while(!pq.isEmpty())
+            result.add(0, pq.poll().getKey());
+        
+        return result;
     }
 }
