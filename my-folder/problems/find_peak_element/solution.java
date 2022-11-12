7cdbb9df6a1aedf97f2ae3b2d0d96f412305a4a2
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-11-12 06:30:21 +0000

    Sync LeetCode submission - Find Peak Element (java)

--- my-folder/problems/find_peak_element/solution.java
+++ my-folder/problems/find_peak_element/solution.java
@@ -11,7 +11,7 @@ class Solution {
           int mid = l + (r-l)/2;
           if(mid >0 && mid <len-1)
           {
-              if(nums[mid] >=nums[mid-1] && nums[mid] >=nums[mid+1])
+              if(nums[mid] >nums[mid-1] && nums[mid] >nums[mid+1])
                   return mid;
               else if(nums[mid] < nums[mid+1])
                    l= mid+1;
