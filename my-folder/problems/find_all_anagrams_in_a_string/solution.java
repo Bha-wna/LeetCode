7fc9f7c3c0090ad6a3240190842b90c5d1597132
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-11-16 18:33:48 +0000

    Sync LeetCode submission - Find All Anagrams in a String (java)

--- my-folder/problems/find_all_anagrams_in_a_string/solution.java
+++ my-folder/problems/find_all_anagrams_in_a_string/solution.java
@@ -1,79 +1,51 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> v = new ArrayList<>();
-
-        if(p.length() > s.length())
-            return v;
-
-        int sizeP = p.length();
         
-        int[] freqP = new int[26];
-        for(int i = 0; i < sizeP; i++){
-            freqP[p.charAt(i) - 'a']++;
+        List<Integer> ans = new ArrayList<>();
+        if(p.length() >s.length())
+            return ans;
+        int lenp = p.length();
+        int [] freqP = new int[26];
+        int [] freqS = new int[26];
+        for(int i=0; i<lenp;i++)
+        {
+            freqP[p.charAt(i)-'a']++;
         }
         
-        int[] freqS = new int[26];
-        for(int i = 0; i < sizeP; i++){
-            freqS[s.charAt(i) - 'a']++;
+        for(int i=0; i<lenp; i++)
+        {
+            freqS[s.charAt(i)-'a']++;
         }
-
+        
         boolean flag = true;
-        for(int i = 0; i < 26; i++){
-            if(freqP[i] != freqS[i]){
+        for(int i=0; i<26;i++)
+        {
+            if(freqS[i] != freqP[i])
+            {
                 flag = false;
                 break;
             }
         }
-
-        if(flag){
-            v.add(0);
-        }
-
-        for(int i = sizeP; i < s.length(); i++){
-            freqS[s.charAt(i-sizeP) - 'a']--;
-            freqS[s.charAt(i)-'a']++;
+        if(flag==true)
+            ans.add(0);
+        
+        //boolean flag = true;
+        for(int j= lenp; j<s.length(); j++)
+        {
+           freqS[s.charAt(j-lenp)-'a']--;
+            freqS[s.charAt(j)-'a']++;
             flag = true;
-            for(int z = 0; z< 26; z++){
-                if(freqP[z] != freqS[z]){
+            for(int i=0; i<26;i++)
+            {
+                if(freqP[i] != freqS[i])
+                {
                     flag = false;
                     break;
                 }
             }
-            if(flag){
-                v.add(i - sizeP + 1);
-            }
+            if(flag==true)
+              ans.add(j-lenp+1);
         }
-        return v; 
+       return ans; 
     }
 }
-//         List<Integer> ans = new ArrayList<>();
-//          int len = p.length();
-//          char[] pmap = new char[26];
-//          for(int i=0; i<p.length(); i++)
-//          {
-//              pmap[p.charAt(i)-'a']++;
-//          } 
-
-//          for(int j=0; j<=s.length()-len; j++)
-//          {
-//              int cnt=0;
-//              String temp = s.substring(j,j+len);
-//              System.out.println(temp);
-//              char[] ch = new char[26];
-//              for(int k=0; k<temp.length(); k++)
-//              {
-//                  ch[temp.charAt(k)-'a']++;
-//              }
-//              for(int i=0; i<ch.length; i++)
-//              {
-//                  if(pmap[i]!= ch[i])
-//                    break;
-//                  else 
-//                    cnt++;  
-//              }
-//              if(cnt ==len)
-//                ans.add(j);
-//          }
-//          return ans;
-//     }
-// }
