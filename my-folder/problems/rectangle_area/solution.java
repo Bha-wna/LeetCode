5839565d587fcec425bac89ad78cb1ce95036b4a
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-11-20 09:03:34 +0000

    Sync LeetCode submission - Rectangle Area (java)

--- my-folder/problems/rectangle_area/solution.java
+++ my-folder/problems/rectangle_area/solution.java
@@ -1,19 +1,45 @@
 class Solution {
-   public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        long area1 = Math.abs(ax1-ax2) * Math.abs(ay1-ay2);
+        long area2 = Math.abs(bx1-bx2) *Math.abs(by1-by2);
+        int ol =0;
+        int ob =0;
         
-        int areaOfSqrA = (C-A) * (D-B);
-         int areaOfSqrB = (G-E) * (H-F);
+        long total_area = area1+ area2;
+        System.out.println(total_area);
         
-        int left = Math.max(A, E);
-        int right = Math.min(G, C);
-        int bottom = Math.max(F, B);
-        int top = Math.min(D, H);
+        int[] length = {ax1,ax2,bx1,bx2};
+        int[] breadth = {ay1,ay2,by1,by2};
+        Arrays.sort(length);
+        Arrays.sort(breadth);
+        ol = Math.abs(length[1]-length[2]);
+        ob = Math.abs(breadth[1] - breadth[2]);
+        long oarea = ol*ob;
         
-        //If overlap
-        int overlap = 0;
-        if(right > left && top > bottom)
-             overlap = (right - left) * (top - bottom);
+        if(ax2<=bx1  || ax1>= bx2 || ay2<=by1 || ay1>= by2)
+            return (int)total_area;
+            
         
-        return areaOfSqrA + areaOfSqrB - overlap;
+        System.out.println(oarea);
+        
+        return (int)(total_area - oarea);
+        
+        //find overlapped region
+        //if a is in right
+        // if(bx1 >= ax1 && bx1<= ax2)
+        // {
+        //     int ol = ax2-bx1;
+        //     //find breadth
+        //     if(by1 >= ay1 && by1 <= ay2)
+        //     {
+        //        ob = ay2-by1
+        //         //ob = Math.abs(ay1-ay2) -(Math.abs(ay1-ay2)-Math.abs(by1-by2));
+        //     }
+        //     else if(by2 >= ay1 && by2 <= ay2)
+        //     {
+        //         ob = ay2 -(ay2 - by2);
+        //     }
+        // }
+        
     }
 }
