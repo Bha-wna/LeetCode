709c8377d01df1e82dabe5393123c18bc215bff4
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-12-27 19:53:29 +0000

    Sync LeetCode submission - Maximum Bags With Full Capacity of Rocks (java)

--- my-folder/problems/maximum_bags_with_full_capacity_of_rocks/solution.java
+++ my-folder/problems/maximum_bags_with_full_capacity_of_rocks/solution.java
@@ -1,53 +1,26 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-       
+        int n = capacity.length, fullBags = 0;
         
-        int need[] = new int[rocks.length];
-        
-        for(int i=0; i< need.length; i++ )
-        {
-            need[i] = capacity[i]-rocks[i];
-        }
-        
-        Arrays.sort(need);
-        for(int i=0; i< need.length; i++)
-        {
-            if(need[i] != 0)
-            {
-                if(need[i]<=additionalRocks)
-                {
-                    
-                   additionalRocks = additionalRocks- need[i];
-                   need[i]=0; 
-                }
+        // Sort bags by the remaining capacity.
+        int[] remainingCapacity = new int[n];
+        for (int i = 0; i < n; ++i)
+            remainingCapacity[i] = capacity[i] - rocks[i];
+        Arrays.sort(remainingCapacity);
+    
+        // Iterate over sorted bags and fill them using additional rocks.
+        for (int i = 0; i < n; ++i) {
+            // If we can fill the current one, fill it and move on.
+            // Otherwise, stop the iteration.
+            if (additionalRocks >= remainingCapacity[i]) {
+                additionalRocks -= remainingCapacity[i];
+                fullBags++;
             }
+            else
+                break;
         }
         
-//         int j=0;
-//         while(capSum != rockSum || additionalRocks != 0)
-//         {
-//             if(need[j] !=0)
-//             {
-//                 if(rocks[j]< capacity[j])
-//                 {
-//                     rocks[j] = rocks[j] +1;
-//                     j++;
-//                     additionalRocks--;
-//                 }
-//             }
-              
-//             for(int i=0; i<rocks.length; i++)
-//             {
-//                  rockSum += rocks[i];
-//             }
-//         }
-        
-        int count=0;
-        for(int i=0; i<need.length; i++)
-        {
-            if(need[i]==0)
-                count++;
-        }
-        return count;
+        // Return `fullBags` after the iteration stops.
+        return fullBags;
     }
 }
