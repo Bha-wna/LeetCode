2e5d9b035fa7c901c996df3e2e394c205d19a521
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2022-12-28 18:57:13 +0000

    Sync LeetCode submission - Tree Node (mysql)

---
+++ my-folder/problems/tree_node/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select id, "Root" as type from tree where p_id is null
+union 
+select id, "Inner" as type from tree where id in (select p_id from tree) and p_id is not null
+union
+select id, "Leaf" as type from tree where id not in (select p_id from tree WHERE p_id IS NOT null) AND p_id IS  NOT  null ;
+
