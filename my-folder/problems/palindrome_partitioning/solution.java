b2f6fd76a1de0a874fac715811168056d8bab7d6
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2023-01-22 19:51:55 +0000

    Sync LeetCode submission - Palindrome Partitioning (java)

--- my-folder/problems/palindrome_partitioning/solution.java
+++ my-folder/problems/palindrome_partitioning/solution.java
@@ -1,36 +1,30 @@
 class Solution {
     public List<List<String>> partition(String s) {
-       
-        List<List<String>> res = new ArrayList<List<String>>();
-        List<String> ds = new ArrayList<>();
-        pp(0,s,res,ds);
+        List<List<String>> res = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        func(0,s,path,res);
         return res;
     }
     
-    public void pp(int ind, String s,List<List<String>> res, List<String> ds )
-    {
-        if(ind==s.length())
-        {   res.add(new ArrayList<>(ds));
-            return;
-        }
-        
-        for(int i=ind; i<s.length(); i++)
-        {
-           if(isPalindrome(ind,i,s))
-           {    ds.add(s.substring(ind,i+1));
-                pp(i+1,s,res,ds);
-                ds.remove(ds.size()-1);
+   public void func(int index, String s , List<String> path, List<List<String>> res ){
+       if(index == s.length()){
+           res.add(new ArrayList<>(path));
+           return;
+       }
+       
+       for(int i=index; i<s.length(); i++){
+           if(isPalindrome(s,index,i)){
+               path.add(s.substring(index,i+1));
+               func(i+1,s,path,res);
+               path.remove(path.size()-1);
            }
-        }
-    }
+       }
+   }
     
-    public boolean isPalindrome(int ind, int i, String s)
-    {
-        while(ind<=i)
-        {
-            if(s.charAt(ind)!= s.charAt(i))
+    boolean isPalindrome(String s, int start, int end){
+        while(start<=end){
+            if(s.charAt(start++) != s.charAt(end--))
                 return false;
-            ind++; i--;
         }
         return true;
     }
