a8a1dc234f514907bd0b1047b36a4e350612f7a6
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2023-02-15 19:40:40 +0000

    Sync LeetCode submission - Count Odd Numbers in an Interval Range (java)

--- my-folder/problems/count_odd_numbers_in_an_interval_range/solution.java
+++ my-folder/problems/count_odd_numbers_in_an_interval_range/solution.java
@@ -1,33 +1,9 @@
 class Solution {
     public int countOdds(int low, int high) {
-        int n;
-        if(low%2 !=0 && high%2 !=0)
-        {
-          n= high-low-1;
-            n= n/2 +2;
+         if (low % 2 == 0 && high % 2 == 0) return (high - low) / 2;
+        else {
+            int first_odd_number = low % 2 == 0 ? low + 1 : low;
+            return (high - first_odd_number) / 2 + 1;
         }
-    /*    else
-        {if(low==high)
-           {
-            if(low%2 !=0)
-                n=1;
-            else
-                n=0;
-           }
-           */
-           else
-           {
-               n=high-low-1;
-               n= n/2+1;
-           }   
-        if(low==high)
-        {
-            if(low%2==0)
-               return 0; 
-            else
-                return 1;   
-        }
-        else
-        return n;
     }
 }
