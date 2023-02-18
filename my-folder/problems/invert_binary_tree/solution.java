916cfd40eddd7b0ac89c8683d33812a3ece18327
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2023-02-18 15:37:38 +0000

    Sync LeetCode submission - Invert Binary Tree (java)

--- my-folder/problems/invert_binary_tree/solution.java
+++ my-folder/problems/invert_binary_tree/solution.java
@@ -15,13 +15,13 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-         if(root==null)
-          return root;
-
-        TreeNode temp = root.right;
         
-        root.right = invertTree(root.left);
-        root.left = invertTree(temp);   
-        return root;
+        if(root ==null)
+            return root;
+        TreeNode n = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(n);
+        
+       return root;
     }
 }
