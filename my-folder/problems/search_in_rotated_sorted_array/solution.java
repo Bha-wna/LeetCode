1300ffeae8607c38e742cdc808ff78e4f5a25f6f
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2023-02-23 16:01:55 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (java)

--- my-folder/problems/search_in_rotated_sorted_array/solution.java
+++ my-folder/problems/search_in_rotated_sorted_array/solution.java
@@ -1,31 +1,30 @@
+//will solve using recursion
 class Solution {
     public int search(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length-1;
         
-        while(l<=r){
-            int mid = l+ (r-l)/2;
-            if(nums[mid]==target)
-                return mid;
+        return helper(nums,target, 0,nums.length-1);
+        
+    }
+    public int helper(int[] nums, int target, int s, int e){
+        if(s >e)
+            return -1;
+        
+        int mid = s+(e-s)/2;
+        if(nums[mid]==target)
+            return mid;
+        if(nums[s]<= nums[mid]){
+            if(target >= nums[s] && target <=nums[mid])
+            {
+               return helper(nums, target, s,mid-1); 
+            }
             else
             {
-                if(nums[l] <= nums[mid])
-                {
-                  if(target<= nums[mid] && target >= nums[l])
-                      r = mid-1;
-                   else
-                       l= mid+1;
-                }
-                else
-                {
-                   if(target>= nums[mid] && target <= nums[r])
-                      l = mid+1;
-                   else
-                       r= mid-1; 
-                }
+                return helper(nums,target,mid+1,e);
             }
-            
         }
-        return -1;
+        if(target >= nums[mid] && target <= nums[e])
+           return helper(nums,target,mid+1,e);
+        else
+            return helper(nums,target,s,mid-1);
     }
 }
