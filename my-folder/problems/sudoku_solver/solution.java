0ab3aa8696ecafda250ec7b46f58f15a18fe9172
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2023-02-28 20:01:24 +0000

    Sync LeetCode submission - Sudoku Solver (java)

--- my-folder/problems/sudoku_solver/solution.java
+++ my-folder/problems/sudoku_solver/solution.java
@@ -1,45 +1,64 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-       solve(board); 
+        validSudoku(board);
+        return;
+        
     }
-    
-    public boolean solve(char[][]board)
-    {
-        for(int row=0; row<board.length; row++)
-        {
-            for(int col=0; col<board[0].length; col++)
-            {
-                if(board[row][col] =='.')
-                {
-                    for(char ch ='1';ch<='9'; ch++)
-                    {
-                        if(isValid(board,row,col,ch))
-                        {   board[row][col] =ch;
-                            
-                          if(solve(board)==true)
-                            return true;
-                          else
-                            board[row][col]='.';
-                        }
-                    }
-                  return false; 
-                }
+    public boolean validSudoku(char[][] board) {
+         int row =-1, col=-1;
+         boolean empty = true;
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+              if(board[i][j] == '.'){
+                  row = i;
+                  col =j;
+                  empty = false;
+                  break;
+              }   
+            }
+            if(empty == false)
+              break;
+        }
+
+        if(empty == true)
+          return true;
+
+        for(int i=1; i<=9; i++){
+            if(isSafe(board, row, col, i)){
+                board[row][col] = (char)(i + '0');
+                if(validSudoku(board)==true)
+                   return true;
+                else   
+                   board[row][col] = '.';
+            }
+        }
+        return false;  
+    }
+
+    public boolean isSafe(char[][]board, int row, int col, int num){
+        
+        //check rowwise
+        for(int i=0; i<9;i++){
+            if(board[i][col]== (char)(num + '0'))
+               return false;
+        }
+
+        //check columnwise
+        for(int i=0; i<9; i++){
+            if(board[row][i] == (char)(num + '0'))
+              return false;
+        }
+
+        //check subbox
+        int rowstart = row- row%3;
+        int colstart = col - col%3;
+        for(int i= rowstart ; i< rowstart +3; i++){
+            for(int j= colstart; j< colstart+3; j++){
+                if(board[i][j] == (char)(num + '0'))
+                  return false;
             }
         }
         return true;
     }
-    
-    public boolean isValid(char[][]board, int row, int col, char ch)
-    {
-        for(int i=0; i<9; i++)
-        {
-            if(board[row][i]==ch)
-                return false;
-            if(board[i][col]==ch)
-                return false;
-            if(board[3*(row/3) + (i/3)][3*(col/3) + (i%3)]==ch)
-                return false;
-        }
-        return true;
-    }    
 }
