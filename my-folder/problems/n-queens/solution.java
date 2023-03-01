ed9176c1a099a5e95fda6444600f6a1556288429
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2023-03-01 17:14:38 +0000

    Sync LeetCode submission - N-Queens (java)

--- my-folder/problems/n-queens/solution.java
+++ my-folder/problems/n-queens/solution.java
@@ -1,54 +1,69 @@
 class Solution {
+    
+    static List<List<String>> ansList ;
     public List<List<String>> solveNQueens(int n) {
-       List<List<String>> ans  = new ArrayList<List<String>>();
-       char[][] board = new char[n][n]; 
-        
-        int[] leftRow = new int[n];
-        int [] upperDiagonal = new int[2*n-1];
-        int [] lowerDiagonal = new int[2*n-1];
-        
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<n; j++)
-                board[i][j] ='.';
+      char[][] board = new char[n][n];
+        for(int i=0; i<n ;i++){
+            for(int j=0; j<n; j++){
+                board[i][j] = '.';
+            }
         }
-        NQueens(0,ans,board,n,leftRow,upperDiagonal,lowerDiagonal);
-        return ans;
+        ansList = new ArrayList<List<String>>();
+        NQueens(n,board,0);
+        return ansList;     
     }
     
-    public void NQueens(int col,List<List<String>> ans,char[][] board, int n,int[] leftRow , int [] upperDiagonal,  int [] lowerDiagonal )
-    {
-        if(col == n)
-        {
-            ans.add(construct(board));
+    public void NQueens(int n, char[][] board, int row){
+        if(row == n){
+            List<String> list = new ArrayList<>();
+            for(char rows []: board){
+                String ans = "";
+                for(char val: rows){
+                    ans += val;
+                }
+                list.add(ans);
+            }
+            ansList.add(list);
             return;
         }
         
-        for(int row =0; row<n; row++)
-        {
-           if(leftRow[row] == 0 &&  lowerDiagonal[row+col]==0 && upperDiagonal[n-1 + col-row]==0)
-           {
-               board[row][col] ='Q';
-               leftRow[row] =1;
-               lowerDiagonal[row+col]=1;
-               upperDiagonal[n-1 + col-row]=1;
-               NQueens(col+1,ans,board,n,leftRow,upperDiagonal,lowerDiagonal); 
-               board[row][col] ='.';
-               leftRow[row] =0;
-               lowerDiagonal[row+col] =0;
-               upperDiagonal[n-1+col-row] =0;   
-           }
+        for(int col =0; col< n; col++){
+            if(isSafe(row, col, board))
+            {
+                board[row][col] = 'Q';
+                NQueens(n, board, row+1);
+                board[row][col] = '.';
+            }
         }
     }
-    
-   public List<String> construct(char[][]board)
-    {
-       List<String> res = new  LinkedList<>();
-       for(int i=0; i<board.length; i++)
-       {
-           String s = new String(board[i]);
-           res.add(s);
-       }
-       return res;
+    public static boolean isSafe(int row, int col, char[][]board){
+       
+        //top-vertical
+        for(int i=row-1; i>=0; i--){
+            if(board[i][col] =='Q')
+                return false;
+        }
+        
+        //diagonal left
+        int x = row-1;
+        int y=col-1;
+        while(x >=0 && y>=0){
+            if(board[x][y] == 'Q')
+                return false;
+            x--;
+            y--;
+        }
+        
+        //diagonal right
+        int a = row-1;
+        int b = col+1;
+        while(a>=0 && b <= board.length-1){
+            if(board[a][b]== 'Q')
+                return false;
+            a--;
+            b++;
+        }
+        
+        return true;
     }
 }
