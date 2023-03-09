6a4d340623c41ee2a0532773ceb0f174629ac5f4
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2023-03-09 17:24:20 +0000

    Sync LeetCode submission - Linked List Cycle II (java)

--- my-folder/problems/linked_list_cycle_ii/solution.java
+++ my-folder/problems/linked_list_cycle_ii/solution.java
@@ -11,16 +11,44 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-      Map<ListNode, Integer> m = new HashMap<>();
-      ListNode temp = head;
-        while(temp != null && temp.next !=null)
+      ListNode slow = head;
+      ListNode fast = head;
+        
+       int l = lengthCycle(slow);
+        if(l == -1)
+            return null;
+        System.out.println(l);
+        for(int i=0; i< l; i++)
         {
-            m.putIfAbsent(temp,0);
-            m.put(temp,m.get(temp)+1);
-            if(m.get(temp) == 2)
-                return temp;
-            temp = temp.next;
+            slow= slow.next;
         }
-        return null;
+        while(fast!= slow)
+        {
+            fast=fast.next;
+            slow= slow.next;
+        }
+        
+        return slow;
     }
+      public int lengthCycle(ListNode head)
+      {
+          ListNode f = head;
+          ListNode s = head;
+          int length = 0;
+          while(f!=null && f.next != null )
+          {
+              f= f.next.next;
+              s= s.next;
+              if(f == s)
+              {  ListNode temp = s;
+                 do
+                 {
+                     temp = temp.next; 
+                      length++;
+                  }while(temp != s);
+                return length;
+              }
+          }
+         return -1; 
+      }
 }
