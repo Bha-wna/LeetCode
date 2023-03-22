4caff018cb20bc0fd8fabffa8e9681a62805b5f3
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2023-03-22 11:16:58 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- my-folder/problems/climbing_stairs/solution.java
+++ my-folder/problems/climbing_stairs/solution.java
@@ -1,18 +1,26 @@
 class Solution {
-//     public int climbStairs(int n) {
-//         if(n==0) return 1;
-//         if(n==1) return 1;
-        
-//         int left = climbStairs(n-1);
-//         int right = climbStairs(n-2);
-        
-//         return left+right;
-//     }
     public int climbStairs(int n) {
-    int a = 1, b = 1;
-    while (n-- > 0)
-        a = (b += a) - a;
-    return a;
-}
-
+        
+        if(n<=2)
+            return n;
+        int[] dp = new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1]+ dp[i-2];
+        }
+        return dp[n];
+        // Arrays.fill(dp,-1);
+        // return helper(n,dp);
+    }
+    
+//     public int helper(int n, int[]dp){
+//         if(n<=2)
+//             return n;
+//         if(dp[n]!=-1)
+//             return dp[n];
+        
+//        return dp[n]= helper(n-1,dp)+ helper(n-2,dp);
+//     }
 }
