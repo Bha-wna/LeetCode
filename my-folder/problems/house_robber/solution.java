d1234d5804888a0b3ed5711f11256116d8bb7802
Bha-wna <75389464+Bha-wna@users.noreply.github.com>
2023-03-23 12:29:55 +0000

    Sync LeetCode submission - House Robber (java)

--- my-folder/problems/house_robber/solution.java
+++ my-folder/problems/house_robber/solution.java
@@ -1,38 +1,33 @@
 class Solution {
-   // static int [] dp ;
     public int rob(int[] nums) {
-      int n = nums.length;
-     // int[] dp = new int[n];
-    // dp[0] =nums[0];
-    int prev = nums[0];
-    int prev2 = 0;
-
-     for(int i=1; i<n; i++)
-     {
-         int take = nums[i];
-         if(i >1)
-           take += prev2;
-         int nontake = 0 + prev;
-         int curr = Math.max(take,nontake);  
-
-         prev2 = prev;
-         prev = curr;
-     }
-     return prev;
+        // int[] dp = new int[nums.length];
+        // dp[0] = nums[0];
+        int prev =nums[0];
+        int prev2 =0;
+        for(int i=1; i<nums.length; i++){
+        
+         int pick =nums[i];
+         if(i>1)
+             pick  += prev2;
+         int notPick = 0 + prev;
+         int curr = Math.max(pick, notPick);
+            
+          prev2 = prev;
+          prev = curr;  
+            
+        }
+        return prev;
+        //return helper(nums.length-1, nums, dp);
     }
+    //public static int helper(int idx, int[]nums, int[] dp){
+        
+//         if(idx==0)
+//             return nums[idx];
+//         if(idx<0)
+//             return 0;
+//         if(dp[idx]!=0)
+//             return dp[idx];
+        
 
-    // public int f(int ind, int[]nums, int[] dp)
-    // {
-    //     if(ind==0)
-    //       return nums[0];
-    //     if(ind<0)
-    //       return 0;
-    //     if(dp[ind] != -1)
-    //       return dp[ind];
-
-    //     int take = nums[ind] + f(ind-2, nums, dp);
-    //     int nontake = 0 + f(ind-1, nums, dp);
-
-    //     return dp[ind] = Math.max(take, nontake); 
-    // }
+//     }
 }
